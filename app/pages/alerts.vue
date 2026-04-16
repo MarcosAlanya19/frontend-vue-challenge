4b7746e67129aee3d563e8785f2dadb51e8e4d9a
Marcos Alanya <asesoralanya19@gmail.com>
2026-04-16 09:15:14 -0500

    feat: add pages example

---
+++ app/pages/alerts.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'main', middleware: ['auth'] })
+</script>
+
+<template>
+  <div class="flex-1 flex items-center justify-center">
+    <UiBaseText size="xl" weight="medium" color="gray-40">Próximamente</UiBaseText>
+  </div>
+</template>
